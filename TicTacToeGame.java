76fb80caf849ccc2ab3a7bf0eb3b11c903f35227
Bathala-Haresh <85885016+Bathala-Haresh@users.noreply.github.com>
2021-07-08 10:18:54 +0530

    Update TicTacToeGame.java

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
-		TicTacToeGame ticTacToeGame=new TicTacToeGame();
-		ticTacToeGame.emptyArray();
+		TicTacToeGame.emptyArray();
 
 	}
 	// method to create an empty array
-	public void emptyArray()
+	public static void emptyArray()
 	{
-		Scanner sc=new Scanner(System.in);
-		int step=1;
 		char [][] javaCharArray=new char[3][3];
 
 		for(int i=0;i<=2;i++)
