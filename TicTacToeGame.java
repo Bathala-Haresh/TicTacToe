cc1a4bc9c8756dc90ca5bce3faecd82f3c5ffb5c
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-08 09:41:19 +0530

    welcome to TicTacToe problem

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,29 +1,9 @@
 package com.workshop;
-import java.util.Scanner;
 
-
-public class TicTacToeGame {
-
-	public static void main(String[] args) {
-		TicTacToeGame ticTacToeGame=new TicTacToeGame();
-		ticTacToeGame.emptyArray();
-
-	}
-	// method to create an empty array
-	public void emptyArray()
+public class TicTacToeGame1 {
+	public static void main(String args[])
 	{
-		Scanner sc=new Scanner(System.in);
-		int step=1;
-		char [][] javaCharArray=new char[3][3];
-
-		for(int i=0;i<=2;i++)
-		{
-			for(int j=0;j<=2;j++)
-			{
-				System.out.println(javaCharArray[i][j]+ " ");
-			}
-
-		}
+		System.out.println("Welcome to TicTacToe Problem");
 	}
 
 }
