8f6d81d825f79cd9d9875ea266abcdd3bd58034b
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-09 08:54:48 +0530

    added program to make move to desired location

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,16 +1,20 @@
 /**
- * As a Player would like to see the board so I can choose the valid cells to make my move during my turn
+ * Ability for user to make a move to a desired location in the board - Select the index from 1 to 9 to make the move. Ensure the index is free
  *
  * @author  Bathala Haresh
  */
 
 package com.workshop;
-
 import java.util.Scanner;
-public class TicTacToeGameUC3
+public class TicTacToeGameUC4
 {
-	public static char[] choosePawn(char[] x,int choice)
+	//method to ask user  
+	public static char[] choosePawn(char[] x)
 	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Choose the Pawn\n1.X\n2.O");
+		System.out.print("Your Pawn Choice:");
+		int choice=sc.nextInt();
 		if(choice==1)
 		{
 			x[1]='O';
@@ -21,43 +25,41 @@ public class TicTacToeGameUC3
 			x[0]='O';
 			x[1]='X';
 		}
-		System.out.println("The Player Pawn is   : "+x[0]);
-		System.out.println("The Computer Pawn is  : "+x[1]);
+		System.out.println("The Player Pawn is"+x[0]);
+		System.out.println("The Player Pawn is"+x[1]);
 
 		return x;
 	}
-	//method to display the Board
+	//checking availability of free space ,if it is * then empty else it is filled with other player
+	public static int checkAvailability(char[] x,int i)
+	{
+		if(x[i]=='*')
+		{
+			return 1;
+		}
+		return 0;
+	}
+	//method to create a visual Game Board
 	public static void showBoard(char[] x)
 	{
 		for(int i=0;i<=8;i++)
 		{
 			System.out.print(x[i]+" ");
-			//we are starting from 0th index so we are incrementing it by 1
 			int a=i+1;
 			if(a%3==0)
 			{
-				System.out.print("\n");
+				System.out.println();
 			}
 		}
 	}
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Choose the Pawn\n1.X\n2.O");
-		System.out.print("Your Pawn Choice:");
-		int choice=sc.nextInt();
-
-		//declaring an array
-		char[] TicTac=new char[9];
-		//filling board with stars
+		char[] ticTacToe=new char[9];
 		for(int i=0;i<=8;i++)
 		{
-			TicTac[i]='*';
+			ticTacToe[i]='*';
 		}
-		//declaring an array
-		char [] pawns=new char[2];
-		//assigning TicTac to showBoard method
-		showBoard(TicTac);
-		pawns=choosePawn(pawns,choice);
-		sc.close();
+		char [] Pawns=new char[2];
+		showBoard(ticTacToe);
+		Pawns=choosePawn(Pawns);
 	}
 }
