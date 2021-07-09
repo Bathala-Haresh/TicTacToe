a5da068c6036704ddf3a8ec2190add8f5980dbd0
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-09 07:55:23 +0530

    added welcome message

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,8 @@
 package com.workshop;
+public class TicTacToeGame {
 
-public class TicTacToeGame1 {
-	public static void main(String args[])
-	{
-		System.out.println("Welcome to TicTacToe Problem");
+	public static void main(String[] args) {
+		System.out.println("welcome to Tic Tac Toe Problem");
+
 	}
-
 }
